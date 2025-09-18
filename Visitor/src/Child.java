14d43c0ee37ea069796cda66e3b55cd0d55a83dd
Javier <schjavier@gmail.com>
2025-09-18 11:53:03 -0300

    add: Visitor Behavioral pattern example

---
+++ Visitor/src/Child.java
@@ -0,0 +1,12 @@
+public class Child extends Person{
+
+    public Child(String name, int edad, String healthStatus) {
+        super(name, edad, healthStatus);
+    }
+
+    @Override
+    public void accept(HealthVisitor visitor){
+        visitor.visit(this);
+    }
+
+}
