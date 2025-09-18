9bef3af21dd32370f1d0f8094b87362f5128ed8b
juliganga <juliganga@gmail.com>
2025-09-18 11:56:56 -0300

    add: Proxy Structural pattern example

---
+++ ejemplo_fachada/src/main/java/org/example/Wizard.java
@@ -0,0 +1,16 @@
+package org.example;
+
+// implementacion de mago
+public class Wizard {
+
+    private final String name;
+
+    public Wizard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
